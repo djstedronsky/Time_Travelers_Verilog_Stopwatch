53969d7492f1f12e1f6d2fd7a95a9026de43e612
hruy1 <58862333+hruy1@users.noreply.github.com>
2020-06-17 17:17:05 -0500

    ADDING 10 milliscond verification and Trigger Detection Test results

--- sv_files/sim_files/rtc_trigger_detection_tb.sv
+++ sv_files/sim_files/rtc_trigger_detection_tb.sv
@@ -32,13 +32,16 @@ always
 
 initial
 begin
+// Set initial value of i_sclk,i_trigger and i_reset_n to 0
 i_sclk=0;
 i_trigger=0;                
 i_reset_n=0;
 
 fork
 //---------------------------------TRIGGER_TC_01 BEGIN----------------------------//
+//check if ‘o_latchcount' and 'o_counterenb' are set to 0  when 'i_reset_n' is 0
 begin: TC_01
+//Set i_trigger and i_reset_n to 0
 #5
 i_trigger=0;                
 i_reset_n=0;
@@ -55,6 +58,7 @@ end:TC_01
 
 
 //---------------------------------TRIGGER_TC_02 BEGIN----------------------------//
+// check if ‘o_countinit’ is set to 1 when ‘i_reset_n’ is 0
 begin:TC_02
 #5
 if(o_countinit==1'b1)begin
@@ -69,6 +73,7 @@ end:TC_02
 //---------------------------------TRIGGER_TC_02 END----------------------------//
 
 //---------------------------------TRIGGER_TC_03_1 BEGIN----------------------------//
+//check if ‘o_countinit’ is set to 0 when ‘i_reset_n’ is 1.
 
 begin:TC_03_DATA_1   
 #10 
@@ -88,15 +93,16 @@ end:TC_03_1
 //---------------------------------TRIGGER_TC_03_1 END----------------------------//
 
 //---------------------------------TRIGGER_TC_04_1 BEGIN----------------------------//
+//set 'i_trigger' to 1 for 2 clock cycles of 'i_sclk'
 begin:TC_04_DATA_1  
 #10 
 i_trigger=1;   
 i_reset_n=1;
 end:TC_04_DATA_1
-
+// Check if o_countenb and o_latchcount are 1
 begin:TC_04_1
 #35
-if(o_countenb == 1)begin
+if(o_countenb == 1 && o_latchcount==1)begin
       $display("Time=>%0d ,i_trigger=>%b,o_countenb=>%b, o_latchcount=>%b [TRIGGER_TC_04 PASS]"
       ,$time,i_trigger,o_countenb,o_latchcount); 
 end else begin
@@ -105,7 +111,7 @@ end else begin
 end
 
 end: TC_04_1
-
+// set i_trigger to 0
 begin:TC_04_DATA_2  
 #35 
 i_trigger=0;   
@@ -115,15 +121,17 @@ end:TC_04_DATA_2
 
 
 //---------------------------------TRIGGER_TC_04_2 BEGIN----------------------------//
+//set 'i_trigger' to 1 for 2 clock cycles of 'i_sclk'
 begin:TC_04_DATA_3  
 #50 
 i_trigger=1;   
 i_reset_n=1;
 end:TC_04_DATA_3
 
+// check if o_countenb and o_latchcount are 0
 begin:TC_04_2
 #75
-if(o_countenb == 0)begin
+if(o_countenb == 0 && o_latchcount == 0)begin
       $display("Time=>%0d ,i_trigger=>%b,o_countenb=>%b, o_latchcount=>%b [TRIGGER_TC_04 PASS]"
       ,$time,i_trigger,o_countenb,o_latchcount); 
 end else begin
@@ -132,7 +140,7 @@ end else begin
 end
 
 end: TC_04_2
-
+//set 'i_trigger' to 0 
 begin:TC_04_DATA_4  
 #75 
 i_trigger=0;   
@@ -142,15 +150,17 @@ end:TC_04_DATA_4
 
 
 //---------------------------------TRIGGER_TC_04_3 BEGIN----------------------------//
+//set 'i_trigger' to 1 for 2 clock cycles of 'i_sclk'
 begin:TC_04_DATA_5  
 #120 
 i_trigger=1;   
 i_reset_n=1;
 end:TC_04_DATA_5
 
+// check if o_countenb and o_latchcount are 1
 begin:TC_04_3
 #145
-if(o_countenb == 1)begin
+if(o_countenb == 1 && o_latchcount == 1)begin
       $display("Time=>%0d ,i_trigger=>%b,o_countenb=>%b, o_latchcount=>%b [TRIGGER_TC_04 PASS]"
       ,$time,i_trigger,o_countenb,o_latchcount); 
 end else begin
@@ -159,7 +169,7 @@ end else begin
 end
 
 end: TC_04_3
-
+// set 'i_trigger' to 0
 begin:TC_04_DATA_6  
 #145 
 i_trigger=0;   
@@ -169,15 +179,17 @@ end:TC_04_DATA_6
 
 
 //---------------------------------TRIGGER_TC_04_4 BEGIN----------------------------//
+//set 'i_trigger' to 1 for 2 clock cycles of 'i_sclk'
 begin:TC_04_DATA_7  
 #180 
 i_trigger=1;   
 i_reset_n=1;
 end:TC_04_DATA_7
 
+// check if o_countenb and o_latchcount are 0
 begin:TC_04_4
 #205
-if(o_countenb == 0)begin
+if(o_countenb == 0 && o_latchcount == 0)begin
       $display("Time=>%0d ,i_trigger=>%b,o_countenb=>%b, o_latchcount=>%b [TRIGGER_TC_04 PASS]"
       ,$time,i_trigger,o_countenb,o_latchcount); 
 end else begin
@@ -186,7 +198,7 @@ end else begin
 end
 
 end: TC_04_4
-
+// set 'i_trigger' to 0
 begin:TC_04_DATA_8  
 #205 
 i_trigger=0;   
